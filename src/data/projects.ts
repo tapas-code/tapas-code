7fc909fe40d9c1e40c5d6202bc5899f5c03edc0d
tapas-code <tapas.code247@gmail.com>
2025-04-01 19:32:31 +0530

    Add Gamex Project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,10 +10,20 @@ export const projects = [
     demo: "https://facesearch-ai.netlify.app",
   },
   {
+    title: "Gamex",
+    description:
+      "An futuristic gaming website with smooth animations, seamless transitions, and a modern, responsive design for an immersive experience.",
+    image: "/gaming.jpg",
+    video: "https://youtu.be/34vnHJz6LWc",
+    tags: ["React.js", "Tailwind CSS", "GSAP"],
+    github: "https://github.com/tapas-code/gamex",
+    demo: "https://gamex-v1.netlify.app",
+  },
+  {
     title: "Real-time Crypto Price Tracker",
     description:
       "A real-time cryptocurrency price monitoring and alerting system using Redis caching and email notifications using nodemailer.",
-    image: "https://img.freepik.com/free-photo/cyberpunk-bitcoin-illustration_23-2151611173.jpg?t=st=1739378844~exp=1739382444~hmac=5d0f25750e7858db164e7544abf39493481383c3a750a597adbc2b7823113b4e&w=1380",
+    image: "/bitcoin.jpg",
     video: "https://youtu.be/0uXQ1SIIvUc",
     tags: ["Node.js", "TypeScript", "Redis", "MongoDB", "CoinGecko API"],
     github: "https://github.com/tapas-code/crypto_monitor_server",
@@ -39,7 +49,7 @@ export const projects = [
     title: "Note-Taking App",
     description:
       "A secure and user-friendly note-taking app with email-based OTP authentication, JWT security, and a mobile-friendly UI.",
-    image: "https://img.freepik.com/free-photo/futuristic-business-scene-with-ultra-modern-ambiance_23-2151003803.jpg?t=st=1739378311~exp=1739381911~hmac=98ba68869a638725897e90e877458d03f0e23bd95bd17b40766f0a941b629001&w=1060",
+    image: "/note.jpg",
     video: "https://youtu.be/M_y_-E7neWQ",
     tags: ["Next.js", "TypeScript", "Node.js", "MongoDB", "Express", "JWT"],
     github: "https://github.com/tapas-code/notes_client",
@@ -49,7 +59,7 @@ export const projects = [
     title: "Quiz.com",
     description:
       "A web-based quiz application with gamification features, fetching quiz data from an API and presenting it through an intuitive and engaging user interface.",
-    image: "https://img.freepik.com/free-photo/neon-gaming-background_23-2151932063.jpg?t=st=1739378439~exp=1739382039~hmac=35b443c99f80cf244062b646bd7b8d5d1416fc3c76b46fc6575c8c0c20d915ba&w=1380",
+    image: "/quiz.jpg",
     video: "https://youtu.be/9aGcq4SxTWI",
     tags: ["React", "TypeScript", "Tailwind CSS", "Gamification"],
     github: "https://github.com/tapas-code/quiz.com",
@@ -59,7 +69,7 @@ export const projects = [
     title: "SAS - Secure Auth System",
     description:
       "A secure authentication system built with Node.js and TypeScript, implementing JWT, OAuth, and a complete forgot password flow.",
-    image: "https://img.freepik.com/free-photo/cybersecurity-concept-collage-design_23-2151877167.jpg?t=st=1739378698~exp=1739382298~hmac=159374a9a0b7dc4cd1ffbb9d35c37fdb5bf9abf0b26b3000986635b4da604c4f&w=1060",
+    image: "/security.jpg",
     video: "https://youtu.be/4eFHWBz7eAc",
     tags: [
       "Node.js",
